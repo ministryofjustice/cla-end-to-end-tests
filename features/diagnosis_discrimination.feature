2bdafe64e94d899b6505f567fd14333b4aabd177
Heather Poole <heather.poole@digital.justice.gov.uk>
2021-10-22 15:52:01 +0100

    Added feature and temp step defs

---
+++ features/diagnosis_discrimination.feature
@@ -0,0 +1,16 @@
+Feature: Correct decision on discrimination diagnosis.
+    Confirms that when a case with the scope diagnosis of 'discrimination,
+    direct discrimination, disability, work' is selected that you
+    get an INSCOPE decision.
+
+Background: Login
+    Given that I am logged in
+
+Scenario: Given an INSCOPE decision when selecting discrimination at work
+    Given I am on the 'case details' page
+    And a user has been created
+    When I select ‘Create Scope Diagnosis'￼
+    And I select the categories Discrimination, Direct Discrimination, Disability, Work￼
+    Then I get an INSCOPE decision￼
+    And select 'Create financial assessment'￼
+    Then I am taken to the Finances tab with the ‘Details’ sub-tab preselected
---
+++ features/steps/diagnosis_discrimination_steps.py
@@ -0,0 +1,26 @@
+from behave import *
+
+
+@when(u'I select ‘Create Scope Diagnosis\'￼')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I select ‘Create Scope Diagnosis\'￼')
+
+
+@when(u'I select the categories Discrimination, Direct Discrimination, Disability, Work￼')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I select the categories Discrimination, Direct Discrimination, Disability, Work￼')
+
+
+@then(u'I get an INSCOPE decision￼')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I get an INSCOPE decision￼')
+
+
+@then(u'select \'Create financial assessment\'￼')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then select \'Create financial assessment\'￼')
+
+
+@then(u'I am taken to the Finances tab with the ‘Details’ sub-tab preselected')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I am taken to the Finances tab with the ‘Details’ sub-tab preselected')
