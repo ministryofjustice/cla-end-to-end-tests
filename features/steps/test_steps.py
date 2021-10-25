42c8313f94ef601e80ae2f0ac9b1d34d15222c47
said-moj <said.shire@digital.justice.gov.uk>
2021-10-25 11:40:49 +0100

    Added steps for going to case detail page

--- features/steps/test_steps.py
+++ features/steps/test_steps.py
@@ -1,3 +1,4 @@
+import re
 from behave import *
 
 
@@ -8,6 +9,7 @@ def step_impl(context):
     login_url = f"{config['cla_frontend_url']}/auth/login/"
     context.helperfunc.open(login_url)
     form = context.helperfunc.find_by_name('login_frm')
+    assert form is not None
     form.find_element_by_name("username").send_keys(config["cla_frontend_operator_username"])
     form.find_element_by_name("password").send_keys(config["cla_frontend_operator_password"])
     form.find_element_by_name("login-submit").click()
@@ -24,17 +26,23 @@ def step_impl(context):
 
 @when(u'I select to \'Create a case\'.')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I select to \'Create a case\'.')
+    context.helperfunc.find_by_id("create_case").click()
 
 
 @then(u'I am taken to the \'case details\' page.')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I am taken to the \'case details\' page.')
+    assert context.helperfunc.find_by_xpath("//header/h1").text == "Case details"
+    current_path = context.helperfunc.get_current_path()
+    assert re.match(r"^/call_centre/\w{2}-\d{4}-\d{4}/diagnosis/$", current_path)
 
 
 @then(u'I select \'Create new user\'.')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I select \'Create new user\'.')
+    btn = context.helperfunc.find_by_name("create-newuser")
+    assert btn is not None
+    btn.click()
+    form = context.helperfunc.find_by_name("personaldetails_frm")
+    assert form.is_displayed()
 
 
 @then(u'enter the client\'s personal details.')
