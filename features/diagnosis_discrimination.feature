d51ddb9b395538ca745ce42a22e3a510c60469f6
Heather Poole <heather.poole@digital.justice.gov.uk>
2021-10-28 12:18:03 +0100

    test has been coded and is passing

--- features/diagnosis_discrimination.feature
+++ features/diagnosis_discrimination.feature
@@ -8,9 +8,8 @@ Background: Login
 
 Scenario: Given an INSCOPE decision when selecting discrimination at work
     Given I am on the 'case details' page
-    And a user has been created
     When I select ‘Create Scope Diagnosis'￼
-    And I select the categories Discrimination, Direct Discrimination, Disability, Work￼
+    And I select the categories Discrimination, Direct Discrimination, Disability, Work
     Then I get an INSCOPE decision￼
     And select 'Create financial assessment'￼
     Then I am taken to the Finances tab with the ‘Details’ sub-tab preselected
--- features/steps/diagnosis_discrimination_steps.py
+++ features/steps/diagnosis_discrimination_steps.py
@@ -1,26 +1,61 @@
 from behave import *
+import re
+
+
+@given(u'I am on the \'case details\' page')
+def step_impl(context):
+    context.helperfunc.find_by_id("create_case").click()
+    assert context.helperfunc.find_by_xpath("//header/h1").text == "Case details"
+    current_path = context.helperfunc.get_current_path()
+    assert re.match(r"^/call_centre/\w{2}-\d{4}-\d{4}/diagnosis/$", current_path)
 
 
 @when(u'I select ‘Create Scope Diagnosis\'￼')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I select ‘Create Scope Diagnosis\'￼')
+    context.helperfunc.find_by_name("diagnosis-new").click()
 
 
-@when(u'I select the categories Discrimination, Direct Discrimination, Disability, Work￼')
+@when(u'I select the categories Discrimination, Direct Discrimination, Disability, Work')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I select the categories Discrimination, Direct Discrimination, Disability, Work￼')
+    form = context.helperfunc.find_by_name('diagnosis-form')
+    assert form.is_displayed()
+
+    # Discrimination
+    context.helperfunc.find_by_xpath("//input[@value='n106']").click()
+    context.helperfunc.find_by_name("diagnosis-next").click()
+    assert context.helperfunc.find_by_class("SummaryBlock-node-n106").is_displayed()
+    
+    # Just clicking next
+    context.helperfunc.find_by_name("diagnosis-next").click()
+
+    # Direct discrimination
+    context.helperfunc.find_by_xpath("//input[@value='n108']").click()
+    context.helperfunc.find_by_name("diagnosis-next").click()
+
+    # Disability
+    context.helperfunc.find_by_xpath("//input[@value='n109']").click()
+    context.helperfunc.find_by_name("diagnosis-next").click()
+
+    # Work
+    context.helperfunc.find_by_xpath("//input[@value='n110']").click()
+    context.helperfunc.find_by_name("diagnosis-next").click()
+    
+    # Makes sure we at the end of the scope assessment
+    assert context.helperfunc.find_by_partial_link_text('Create financial assessment').is_displayed()
 
 
 @then(u'I get an INSCOPE decision￼')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I get an INSCOPE decision￼')
+    text = context.helperfunc.find_by_name('diagnosis-form').text
+    assert "INSCOPE" in text
 
 
 @then(u'select \'Create financial assessment\'￼')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then select \'Create financial assessment\'￼')
+    context.helperfunc.find_by_partial_link_text('Create financial assessment').click()
 
 
 @then(u'I am taken to the Finances tab with the ‘Details’ sub-tab preselected')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I am taken to the Finances tab with the ‘Details’ sub-tab preselected')
+    selected_tab = context.helperfunc.find_by_css_selector("li[class='Tabs-tab is-active']")
+    assert 'Finances' in selected_tab.text
