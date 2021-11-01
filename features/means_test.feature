c8ef00c13840d742e742b0835337a9d4c07bde03
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2021-11-01 14:48:50 +0000

    Create means test feature file and add previous test scenario
    Added Heather's test on discrimination by including them in the first two test cases. Essentially, so I can continue where Heather left off

---
+++ features/means_test.feature
@@ -0,0 +1,31 @@
+Feature: Means test assessment - universal credit
+    Confirming that when a person:
+
+    - Does not have a partner
+    - Is aged 60 or over
+    - Is on universal credit benefits
+    - Has no savings in the bank
+    - Has no investments, shares or ISAs
+    - Has no valuable items worth over £500 each
+    - Has no money owed to them
+
+    That they are eligble for legal aid
+
+Background: Login
+    Given that I am logged in
+
+@means_test
+Scenario: Successful means test assessment resulting in user being eligible for legal aid
+    Given that I am on the case details page
+    And I am on the Finances tab with the ‘Details’ sub-tab preselected
+    When I do not have a partner
+    And I am not aged 60 or over
+    And I am on universal credit benefits
+    And I move onto Finances sub-tab
+    And I have no savings in the bank
+    And I have no investments, shares or ISAs
+    And I have no valuable items worth over £500 each
+    And I have no money owed to me
+    And I select Save assessment
+    Then I am given a message 'The means test has been saved. The current result is eligible for Legal Aid'
+    And the 'Diversity' and 'Assign' tabs become available
---
+++ features/steps/means_test.py
@@ -0,0 +1,73 @@
+from behave import *
+import re
+
+
+@given(u'that I am on the case details page')
+def step_impl(context):
+    context.execute_steps(u'''
+        Given I select to 'Create a case'
+    ''')
+
+@step(u'I am on the Finances tab with the ‘Details’ sub-tab preselected')
+def step_impl(context):
+    context.execute_steps(u'''
+        When I select ‘Create Scope Diagnosis'
+        And I select the categories Discrimination, Direct Discrimination, Disability, Work
+        Then I get an INSCOPE decision
+        And select 'Create financial assessment'
+        Then I am taken to the Finances tab with the ‘Details’ sub-tab preselected
+    ''')
+
+@when(u'I do not have a partner')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I do not have a partner')
+
+
+@when(u'I am not aged 60 or over')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I am not aged 60 or over')
+
+
+@when(u'I am on universal credit benefits')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I am on universal credit benefits')
+
+
+@when(u'I move onto Finances tab')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I move onto Finances tab')
+
+
+@when(u'I have no savings in the bank')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I have no savings in the bank')
+
+
+@when(u'I have no investments, shares or ISAs')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I have no investments, shares or ISAs')
+
+
+@when(u'I have no valuable items worth over £500 each')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I have no valuable items worth over £500 each')
+
+
+@when(u'I have no money owed to me')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I have no money owed to me')
+
+
+@when(u'I select Save assessment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I select Save assessment')
+
+
+@then(u'I am given a message \'The means test has been saved. The current result is eligible for Legal Aid\'')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I am given a message \'The means test has been saved. The current result is eligible for Legal Aid\'')
+
+
+@then(u'the \'Diversity\' and \'Assign\' tabs become available')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the \'Diversity\' and \'Assign\' tabs become available')
