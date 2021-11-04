ef86199722dabb4be03aa0ec71e820c6ed895709
Heather Poole <heather.poole@digital.justice.gov.uk>
2021-11-04 11:50:57 +0000

    added final bit to go through elibility and pick up alert

--- features/steps/assign_incomplete_case.py
+++ features/steps/assign_incomplete_case.py
@@ -1,4 +1,5 @@
 from behave import *
+from selenium.webdriver.common.action_chains import ActionChains
 from create_eligible_finances import create_eligible_finance
 
 
@@ -30,18 +31,46 @@ def step_impl(context):
 @given(u'I am on the Diversity tab')
 def step_impl(context):
     create_eligible_finance(context)
+    context.helperfunc.find_by_partial_link_text("Diversity").click()
+    assert "Gender" in context.helperfunc.find_by_css_selector("h2[class='FormBlock-label ng-binding']").text
 
 
 @when(u'I select \'Prefer not say\' for all diversity questions')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I select \'Prefer not say\' for all diversity questions')
+    page = context.helperfunc
+    radio = page.find_by_css_selector("input[name='gender'][value='Prefer not to say']")
+    radio.click()
+    page.find_by_name("diversity-next").click()
+
+    assert "Ethnic origin" in page.find_by_css_selector("h2[class='FormBlock-label ng-binding']").text
+    radio = page.find_by_css_selector("input[name='ethnicity'][value='Prefer not to say']")
+    radio.click()
+    page.find_by_name("diversity-next").click()
+
+    assert "Disabilities" in page.find_by_css_selector("h2[class='FormBlock-label ng-binding']").text
+    radio = page.find_by_css_selector("input[name='disability'][value='PNS - Prefer not to say']")
+    radio.click()
+    page.find_by_name("diversity-next").click()
+
+    assert "Religion / belief" in page.find_by_css_selector("h2[class='FormBlock-label ng-binding']").text
+    radio = page.find_by_css_selector("input[name='religion'][value='Prefer not to say']")
+    radio.click()
+    page.find_by_name("diversity-next").click()
+
+    assert "Sexual orientation" in page.find_by_css_selector("h2[class='FormBlock-label ng-binding']").text
+    radio = page.find_by_css_selector("input[name='sexual_orientation'][value='Prefer Not To Say']")
+    radio.click()
+    page.find_by_name("diversity-save").click()
+
+    assert "The client has completed diversity monitoring." in page.find_by_class("SummaryBlock-content").text
 
 
 @when(u'select the Assign tab')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When select the Assign tab')
+    context.helperfunc.find_by_partial_link_text("Assign").click()
 
 
 @then(u'I get a message with the text "Case notes must be added to close a case"')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I get a message with the text "Case notes must be added to close a case"')
+    alert = context.helperfunc.find_by_css_selector("div[class='modal-dialog '")
+    assert "Case notes must be added to close a case" in alert.text
--- features/steps/create_eligible_finances.py
+++ features/steps/create_eligible_finances.py
@@ -7,8 +7,6 @@ def create_eligible_finance(context):
     # don't actually know if this a genuine profile, but gives you the
     # inscope scope
     page.find_by_partial_link_text("Finances").click()
-    # selected_tab = context.helperfunc.find_by_css_selector("li[class='Pills-pill is-active']")
-    # assert 'Details' in selected_tab.text
     # This section is the 'Details' tab
     # click no to having a partner
     page.find_by_id("id_your_details-has_partner_1").click()
@@ -26,7 +24,17 @@ def create_eligible_finance(context):
     page.find_by_css_selector("input[name='your_details-specific_benefits-employment_support'][value='false']").click()
 
     # This clicks to the 'finance' tab ... in the finance tab. 
-    finance_tab = page.find_by_xpath("//*[@id='pills-section-list']/li[2]")
     actions = ActionChains(page.driver())
+    finance_tab = page.find_by_xpath("//*[@id='pills-section-list']/li[2]")
     actions.move_to_element(finance_tab).perform()
     finance_tab.click()
+
+    page.find_by_css_selector("input[ng-model='eligibility_check.you.savings.bank_balance']").send_keys('0')
+
+    page.find_by_css_selector("input[ng-model='eligibility_check.you.savings.investment_balance']").send_keys('0')
+
+    page.find_by_css_selector("input[ng-model='eligibility_check.you.savings.asset_balance']").send_keys('0')
+
+    page.find_by_css_selector("input[ng-model='eligibility_check.you.savings.credit_balance']").send_keys('0')
+
+    page.find_by_name("save-means-test").click()
