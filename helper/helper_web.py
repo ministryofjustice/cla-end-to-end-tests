aa1ddcebc43c8c799ae70bd310807b735e28697c
said-moj <said.shire@digital.justice.gov.uk>
2021-11-04 07:39:27 +0000

    WIP - Adding usage of chromedriver in host machine and use local chrome instance

--- helper/helper_web.py
+++ helper/helper_web.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from helper.helper_base import HelperFunc
- 
+
+
 def get_browser(browser):
     if browser == "chrome":
-        return HelperFunc(webdriver.Remote("http://seleniumchrome:4444/wd/hub", DesiredCapabilities.CHROME))
+        return HelperFunc(webdriver.Remote("http://localhost:9515", DesiredCapabilities.CHROME))
