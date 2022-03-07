e3e3f0d38fd4643d10d8df2b80f32339fcefd5d6
said-moj <said.shire@digital.justice.gov.uk>
2022-03-07 10:27:37 +0000

    Add CHS operator api credentials to end-to-end container

--- behave/features/constants.py
+++ behave/features/constants.py
@@ -3,6 +3,14 @@ CLA_BACKEND_URL = os.environ.get("CLA_E2E_BACKEND_URL")
 CLA_FRONTEND_URL = os.environ.get("CLA_E2E_FRONTEND_URL")
 CLA_PUBLIC_URL = os.environ.get("CLA_E2E_PUBLIC_URL")
 SELENIUM_WEB_DRIVER_URL = os.environ.get("CLA_E2E_SELENIUM_WEB_DRIVER_URL")
+CLA_NUMBER = "0345 345 4 345"
+CALL_CENTRE_ZONE = {
+    "client_id": os.environ.get("CALL_CENTRE_CLIENT_ID"),
+    "client_secret": os.environ.get("CALL_CENTRE_SECRET_ID"),
+    "grant_type": "password",
+    "username": os.environ.get("CALL_CENTRE_TEST_USER"),
+    "password": os.environ.get("CALL_CENTRE_TEST_USER")
+}
 
 CLA_FRONTEND_PERSONAL_DETAILS_FORM = {
     "full_name": "Bob Merchandise",
