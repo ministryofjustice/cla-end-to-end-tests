821f6664e0c53dcb0951801332570c738b96921b
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2022-03-08 12:05:10 +0000

    changed method so no delay on checking for there is a problem message and also changed from valueerror to assertionerror

--- behave/features/steps/cla_in_scope.py
+++ behave/features/steps/cla_in_scope.py
@@ -2,7 +2,7 @@ from hashlib import new
 from behave import *
 from selenium.webdriver.support.ui import WebDriverWait
 from features.constants import CLA_PUBLIC_URL
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
 def assert_header_on_page(title, context):
     heading = context.helperfunc.find_by_xpath('//h1')
@@ -19,22 +19,20 @@ def wait_until_page_is_loaded(path, context):
 @step(u'I click continue')
 def step_click_continue(context):
     # click on the continue button
-    # remember the current page
-    current_page = context.helperfunc.get_current_path()
     continue_button = context.helperfunc.find_by_id("submit-button")
     assert continue_button is not None
     continue_button.click()
     # did the form get submitted correctly?
-    # check for 'there is a problem' but need to use wait_until as may take time to change page
+    # check for 'there is a problem'
     try:
-        confirmation_text_element = context.helperfunc.find_by_css_selector(".govuk-error-summary")
+        confirmation_text_element = context.helperfunc.driver().find_element_by_css_selector(".govuk-error-summary")
         if confirmation_text_element is not None:
             assert confirmation_text_element.text.startswith("There is a problem")
-            raise ValueError(f"There is a problem with form")   
-    except TimeoutException as ex:
+            raise AssertionError(f"There is a problem with submitting the form")   
+    except NoSuchElementException as ex:
         # this will error because we actually moved off the page which is actually what we want
-        assert current_page != context.helperfunc.get_current_path()
-
+        pass
+        
 # this is a shared step
 @step(u'I am taken to the "{header}" page located on "{page}"')
 def step_check_page(context, page, header):
@@ -100,6 +98,7 @@ def step_impl_means_test(context):
 def step_choose_universal_credit(context):
     # check value of universal_credit checkbox
     check_box_universal_credit = context.helperfunc.driver().find_element_by_id("benefits-4")
+    check_box_universal_credit_2 = context.helperfunc.driver().find_element_by_id("benefits-4")
     assert check_box_universal_credit is not None
     # click on universal credit
     check_box_universal_credit.click()
