31106422d37ccd789fc20cd87b2b73078f417d8c
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2022-03-29 13:59:48 +0100

    changed local-yaml to point at branch while create fixture

--- behave/run_test_local.sh
+++ behave/run_test_local.sh
@@ -3,5 +3,6 @@ export DOCKER_BUILDKIT=0
 docker-compose down
 docker-compose -f docker-compose.yml -f docker-compose.local.yml run start_applications
 docker-compose exec clabackend bin/create_db.sh
+# docker-compose exec -t clabackend python "manage.py loaddata test_special_provider_case.json"
 
-docker-compose up --build cla-end-to-end
+# docker-compose up --build cla-end-to-end
