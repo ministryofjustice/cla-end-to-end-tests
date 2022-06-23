36b94ad28a27e4234444fd3a53b41f0814df051e
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2022-06-23 15:55:43 +0100

    Refactor steps for going through diagnosis check and move pass means test step into different file.

--- behave/features/steps/cla_eligible_callback_or_confirmation.py
+++ behave/features/steps/cla_eligible_callback_or_confirmation.py
@@ -11,46 +11,12 @@ def assert_form_input_element(callback_form, element_id, value):
     assert element.get_attribute('value') == value
 
 
-@given(u'I have passed the means test')
-def step_means_test_page(context):
-    # The next steps are the steps that pass the means test
-   context.execute_steps(u'''
-        Given I am taken to the "Choose the area you most need help with" page located on "/scope/diagnosis/"
-        When I select the category Education
-        And the category Special Educational needs
-        Then I am taken to the "Legal aid is available for this type of problem" page located on "/legal-aid-available"
-        And I click on the 'Check if you qualify financially' button 
-        And I am taken to the "About you" page located on "/about"
-        And I <answer> the <question> 
-            | question                                                   | answer |
-            | Do you have a partner?                                     | No     |
-            | Do you receive any benefits (including Child Benefit)?     | Yes    |
-            | Do you have any children aged 15 or under?                 | No     |
-            | Do you have any dependants aged 16 or over?                | No     |
-            | Do you own any property?                                   | No     |
-            | Are you employed?                                          | No     |
-            | Are you self-employed?                                     | No     |
-            | Are you or your partner (if you have one) aged 60 or over? | No     |
-            | Do you have any savings or investments?                    | No     |
-            | Do you have any valuable items worth over £500 each?       | No     |
-        # All steps that are clicking continue written in identical format so can reuse code
-        And I click continue
-        And I am taken to the "Which benefits do you receive?" page located on "/benefits"
-        And I select 'Universal Credit' from the list of benefits
-        And I click continue 
-        And I am taken to the "Review your answers" page located on "/review"
-        # this is actually click confirm
-        And I click continue
-        Then I am taken to the "Contact Civil Legal Advice" page located on "/result/eligible"
-        ''')
-
-
 @given(u'I enter my personal details')
 def step_enter_personal_details(context):
     personal_details_form = CLA_MEANS_TEST_PERSONAL_DETAILS_FORM
     context.callback_form = context.helperfunc.find_by_xpath("//form")
     context.form_values = {}
-    for name, value,  in personal_details_form.items():
+    for name, value, in personal_details_form.items():
         context.form_values[name] = value['form_element_value']
         assert_form_input_element(context.callback_form, value['form_element_id'], value['form_element_value'])
 
