3406bda76a14f71c38f2cf64a578725f1aced372
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2022-08-02 16:29:23 +0100

    Created scenario to create new operator user

---
+++ behave/features/foxadmin_users.feature
@@ -0,0 +1,32 @@
+Feature: User journeys for Fox admin or cla_backend to update the users staff status
+
+Background: Log in to the Fox admin as a user within the super users group
+    Given that I am logged in as "FOX_ADMIN_GENERAL_USER"
+
+#Journey P11 Child 1 Navigate to users authentication page and assign user staff status (LGA-1863)
+@fox-assign
+Scenario: Navigate to users authentication page and assign user staff status
+Given I select the link "Users"
+When I select a non-staff user from the list
+And I am taken to the user's details page
+And I select Staff status under permissions
+And I select save
+Then the users details are saved and I am taken back to the list of users
+And I select the link "Log out"
+
+#Journey P11 Child 2 Create an operator user (LGA-1882)
+@fox-create-user
+Scenario: Create an operator user
+Given I select the link "Operators"
+And I choose to "Add operator"
+And I am taken to the "Add operator" page located on "/admin/call_centre/operator/add/"
+And I <provide> the <details> to create a user
+        | details                    | provide           |
+        | Username:                  | elvis.presley     |
+        | Password:                  | rockandroll       |
+        | Password confirmation:     | rockandroll       |
+And I select 'Is active’
+And I choose to "save"
+Then the new operator user is created
+And I am taken to the list of operators page
+And I select the link "Log out"
--- behave/features/steps/foxadmin.py
+++ behave/features/steps/foxadmin.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 import datetime
 from selenium.webdriver.support.ui import WebDriverWait
 import os
-from features.constants import MINIMUM_WAIT_UNTIL_TIME
+from features.constants import MINIMUM_WAIT_UNTIL_TIME, CLA_BACKEND_USER_TO_ASSIGN_STATUS_TO, CLA_BACKEND_USER_TO_ASSIGN_STATUS_TO_PK, FOX_ADMIN_NEW_OPERATOR
+from features.steps.common_steps import wait_until_page_is_loaded, assert_header_on_page
 
 
 @step(u'I enter a date range')
@@ -77,6 +78,112 @@ def step_impl(context):
     wait = WebDriverWait(context.helperfunc.driver(), MINIMUM_WAIT_UNTIL_TIME)
     str_error = f'No downloaded report for {file_name} in {context.download_dir}'
     wait.until(WaitForReportToBeDownloaded(file_name), message=str_error)
+
+
+@step(u'I select a non-staff user from the list')
+def step_impl(context):
+    # just click on a user that we know has non-staff status
+    context.execute_steps(f'''
+    Given I select the link "{CLA_BACKEND_USER_TO_ASSIGN_STATUS_TO}"
+    ''')
+
+
+@when(u'I am taken to the user\'s details page')
+def step_impl(context):
+    # user details page is at /admin/auth/user/{pk}
+    page = f"/admin/auth/user/{CLA_BACKEND_USER_TO_ASSIGN_STATUS_TO_PK}/"
+    wait_until_page_is_loaded(page, context)
+    assert_header_on_page("Change user", context)
+
+
+@step(u'I select Staff status under permissions')
+def step_impl(context):
+    # click on the staff status checkbox
+    xpath = f"//div[@class='checkbox-row']/input[@id='id_is_staff']"
+    context.helperfunc.find_by_xpath(xpath).click()
+    # for some reason this doesn't work
+    # context.helperfunc.click_button(By.XPATH, xpath)
+
+
+@step(u'I select save')
+def step_impl(context):
+    context.helperfunc.click_button(By.NAME, "_save")
+
+
+@step(u'the users details are saved and I am taken back to the list of users')
+def step_impl(context):
+    # check that you have moved back to the list of users page
+    page = f"/admin/auth/user/"
+    wait_until_page_is_loaded(page, context)
+    assert_header_on_page("Select user to change", context)
+    # check there is a message that says the user was changed successfully
+    link_text = 'The user "test_staff" was changed successfully'
+    xpath = f"//ul/li[@class='success'][contains(text(), '{link_text}')]"
+    assert len(context.helperfunc.find_many_by_xpath(xpath)) > 0, f"Cannot find success message"
+
+
+@step(u'I choose to "{action}"')
+def step_impl(context, action):
+    if action == "Add operator":
+        xpath = ".//li/a[@class='addlink']"
+    elif action == "save":
+        xpath = ".//input[@name='_save']"
+    else:
+        raise ValueError(f"Can not process this action. You entered {action}")
+    # find the button and click it
+    context.helperfunc.click_button(By.XPATH, xpath)
+
+
+@step(u'I <provide> the <details> to create a user')
+def step_impl(context):
+    # Find the question by details
+    # Find corresponding input and insert value from provide
+    for row in context.table:
+        text = row['details']
+        value = row['provide']
+        xpath = f".//label[text()='{text}']/following-sibling::input"
+        context.helperfunc.find_by_xpath(xpath).send_keys(value)
+
+
+@step(u'I select \'Is active’')
+def step_impl(context):
+    radio_input = context.helperfunc.find_by_css_selector(f"input[name='is_active']")
+    radio_input.click()
+    assert radio_input.get_attribute("checked") == "true"
+
+
+@step(u'the new operator user is created')
+def step_impl(context):
+    # you are returned to the "select operator to change page"
+    # the user just created exists
+    # make sure that there are no errors on this page
+    # are we still on the same page?
+    if context.helperfunc.get_current_path() == "/admin/call_centre/operator/add/" and context.helperfunc.driver().find_element_by_xpath(f"//p[@class='errornote']") is not None:
+        assert False, "There are errors creating that user"
+    else:
+        assert True
+
+
+@step(u'I am taken to the list of operators page')
+def step_impl(context):
+    context.execute_steps('''
+    Then I am taken to the "Select operator to change" page located on "/admin/call_centre/operator/"''')
+    # the user just created exists - look for the created user in the table
+    xpath = f".//table[@id='result_list']/tbody/tr[th/a[text()='{FOX_ADMIN_NEW_OPERATOR}']]"
+    # .//table[@id='result_list']/tbody/tr[th/a[text()='elvis.presley']]
+    assert context.helperfunc.find_by_xpath(xpath) is not None, f"cannot find row with user {FOX_ADMIN_NEW_OPERATOR}"
+    # now check that it is active but not a manager or a superuser
+    image_path_yes = "/static/admin/img/icon-yes.gif"
+    image_path_no = "/static/admin/img/icon-no.gif"
+    xpath_is_active = "//tr/td[@class='field-is_active_display']/img"
+    xpath_is_manager = "//tr/td[@class='field-is_manager']/img"
+    xpath_is_superuser = "//tr/td[@class='field-is_cla_superuser']/img"
+    assert image_path_yes in context.helperfunc.find_by_xpath(xpath_is_active).get_attribute("src"),\
+        f"User should be active"
+    assert image_path_no in context.helperfunc.find_by_xpath(xpath_is_manager).get_attribute("src"), \
+        f"User should not be manager"
+    assert image_path_no in context.helperfunc.find_by_xpath(xpath_is_superuser).get_attribute("src"), \
+        f"User should not be superuser"
 
 
 
