2d9137eea0cc03a531b7430fbd7520cdd753fac5
said-moj <45761276+said-moj@users.noreply.github.com>
2023-02-08 13:30:14 +0000

    Include --database-snapshot-enabled option for local end-to-end test runner

--- behave/run_test_local.sh
+++ behave/run_test_local.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=0
 
+while [ -n "$1" ]; do # while loop starts
+	case "$1" in
+	--database-snapshot-enabled)
+
+	  echo "Running with database snapshot enabled"
+	  export DATABASE_SNAPSHOT_ENABLED=True
+	;; # Message for -a option
+	esac
+	shift
+done
+
 docker-compose down
 docker-compose -f docker-compose.yml run start_applications
 docker-compose exec clabackend bin/create_db.sh
