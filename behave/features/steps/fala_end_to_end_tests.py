f9c048c735d7b75095de3a1d4241aa062e4d3510
Sarahyjja <sarahyjja@github.com>
2023-02-27 17:19:09 +0000

    LGA-2351: ♻️ Refactoring

--- behave/features/steps/fala_end_to_end_tests.py
+++ behave/features/steps/fala_end_to_end_tests.py
@@ -13,9 +13,7 @@ def assert_result_page(context, expected_url, expected_title, expected_results=F
     result_container_xpath = context.helperfunc.find_by_xpath(
         '//div[@class="search-results-container"]'
     )
-    result_number_paragraph = context.helperfunc.find_by_xpath(
-        '//section/p[@class="govuk-body"]'
-    )
+    result_count = context.helperfunc.find_by_xpath('//section/p[@class="govuk-body"]')
 
     assert (
         current_url == expected_url
@@ -26,12 +24,10 @@ def assert_result_page(context, expected_url, expected_title, expected_results=F
     assert (
         result_container_xpath is not None
     ), "Could not find search results container element"
-    assert (
-        result_number_paragraph is not None
-    ), "Could not find result number paragraph element"
+    assert result_count is not None, "Could not find result number paragraph element"
 
     if expected_results:
-        context.results = int(result_number_paragraph.text.split()[0])
+        context.results = int(result_count.text.split()[0])
 
 
 @step("I am on the Find a legal aid adviser homepage")
@@ -41,27 +37,31 @@ def step_impl_homepage(context):
     title_xpath = context.helperfunc.find_by_xpath(
         "//html/body/div/main/div/div/h1"
     ).text.replace("\n", " ")
+
     assert title_xpath == FALA_HEADER
 
 
 @step('I provide the "{location}" details')
-def step_impl_input_location(context, location):
-    input_id = context.helperfunc.find_by_id("id_postcode")
-    input_id.send_keys(location)
-    assert input_id.get_attribute("value") == location
+def step_impl_input_location_provided(context, location):
+    input_location_id = context.helperfunc.find_by_id("id_postcode")
+    input_location_id.send_keys(location)
+
+    assert input_location_id.get_attribute("value") == location
 
 
 @step('I provide an organisation name "{organisation}"')
-def step_impl_input_organisation(context, organisation):
-    input_id = context.helperfunc.find_by_id("id_name")
-    input_id.send_keys(organisation)
-    assert input_id.get_attribute("value") == organisation
+def step_impl_input_organisation_provided(context, organisation):
+    input_organisation_id = context.helperfunc.find_by_id("id_name")
+    input_organisation_id.send_keys(organisation)
+
+    assert input_organisation_id.get_attribute("value") == organisation
 
 
-@step("I select the 'search' button on the FALA homepage")
-def step_impl_click_search(context):
+@step('I select the "search" button on the FALA homepage')
+def step_impl_search_clicked(context):
     search_button = context.helperfunc.find_by_id("searchButton")
     assert search_button is not None
+
     search_button.click()
 
 
@@ -76,27 +76,29 @@ def step_impl_result_page_with_location_only(context, location):
 
 
 @step('I browse through the filter categories and select "{filter_label}"')
-def step_impl_click_checkbox_filter(context, filter_label):
-    checkbox_input = context.helperfunc.find_by_css_selector(
+def step_impl_checkbox_filter_clicked(context, filter_label):
+    checkbox = context.helperfunc.find_by_css_selector(
         f"input[type='checkbox'][value='{filter_label}']"
     )
-    checkbox_input.click()
-    assert checkbox_input.get_attribute("checked") == "true"
+    checkbox.click()
+
+    assert checkbox.get_attribute("checked") == "true"
 
 
-@step("I select the 'Apply filter' button")
-def step_impl_apply_filter(context):
-    apply_filter_button = context.helperfunc.find_by_name("filter")
-    assert apply_filter_button is not None
-    apply_filter_button.click()
+@step('I select the "Apply filter" button')
+def step_impl_apply_filter_on_homepage(context):
+    filter_button = context.helperfunc.find_by_name("filter")
+    assert filter_button is not None
+
+    filter_button.click()
 
 
 @step(
-    'the result page containing "{location}" is updated to apply the filter "{filter_label}"'
+    'the result page containing "{location}" is updated to apply a filter "{filter_label}"'
 )
-def step_impl_result_page_with_location_filter(context, location, filter_label):
-    location_url_string = location.replace(" ", "+")
-    expected_url = f"{CLA_FALA_URL}/?postcode={location_url_string}&name=&categories={filter_label}&filter="
+def step_impl_apply_filter_on_result_page(context, location, filter_label):
+    location_format = location.replace(" ", "+")
+    expected_url = f"{CLA_FALA_URL}/?postcode={location_format}&name=&categories={filter_label}&filter="
     expected_title = FALA_HEADER
 
     assert_result_page(context, expected_url, expected_title)
@@ -106,6 +108,7 @@ def step_impl_result_page_with_location_filter(context, location, filter_label):
 def step_impl_error_shown_on_page(context):
     alert = context.helperfunc.find_by_css_selector(".alert-message")
     assert alert is not None
+
     expected_text = "No results\nThere are no results matching your search criteria."
     assert alert.text == expected_text
 
@@ -113,26 +116,33 @@ def step_impl_error_shown_on_page(context):
 @step(
     'I am taken to the page corresponding to the "{location}" "{organisation}" search result'
 )
-def step_impl_result_page_with_location_organisation(context, location, organisation):
-    organisation_url_string = organisation.replace(" ", "+")
-    location_url_string = location.replace(" ", "+")
-    expected_url = f"{CLA_FALA_URL}/?postcode={location_url_string}&name={organisation_url_string}&search="
+def step_impl_result_page_with_location_organisation_provided(
+    context, location, organisation
+):
+    organisation_format = organisation.replace(" ", "+")
+    location_format = location.replace(" ", "+")
+    expected_url = (
+        f"{CLA_FALA_URL}/?postcode={location_format}&name={organisation_format}&search="
+    )
     expected_title = FALA_HEADER
 
     assert_result_page(context, expected_url, expected_title)
 
 
 @step("{count:d} result is visible on the results page")
-def step_impl_count_results_visible_on_results_page(context, count):
+def step_impl_count_results_displayed_on_results_page(context, count):
     count = int(count)
-    listitems = context.helperfunc.find_many_by_xpath("//ul[@Class='org-list']/li")
-    assert listitems is not None
-    list_count = len(listitems)
+    list_results_items = context.helperfunc.find_many_by_xpath(
+        "//ul[@Class='org-list']/li"
+    )
+    assert list_results_items is not None
+
+    list_count = len(list_results_items)
     assert list_count == count, f"actual count is {list_count}"
 
 
 @step('I select the language "{language}" with value "{code_indicator}"')
-def step_impl_select_language(context, language, code_indicator):
+def step_impl_language_selection(context, language, code_indicator):
     def select_text(*args):
         Select(context.helperfunc.find_by_xpath("//select")).select_by_visible_text(
             f"{language}"
@@ -159,23 +169,24 @@ def step_impl_select_language(context, language, code_indicator):
 
 
 @step(
-    'the page is updated to "{code_indicator}" and title starts with "{title_text_starts_with}"'
+    'the page is updated to "{code_indicator}" and the title starts with "{title_starts_with}"'
 )
-def step_impl_translated(context, code_indicator, title_text_starts_with):
+def step_impl_page_translated(context, code_indicator, title_starts_with):
     updated_page = context.helperfunc.find_by_xpath("/html").get_attribute("lang")
-    updated_title_gui = context.helperfunc.find_by_xpath(
+    dom_updated_title = context.helperfunc.find_by_xpath(
         "//html/body/div/main/div/div/h1"
     ).text
 
     assert updated_page == f"{code_indicator}"
-    assert updated_title_gui.startswith(f"{title_text_starts_with}")
+    assert dom_updated_title.startswith(f"{title_starts_with}")
 
 
 @step("There are less results visible on the results page")
-def step_impl_fewer_results_returned(context):
+def step_impl_less_results_returned(context):
     total_results = context.helperfunc.find_by_xpath(
         '//section/p[@class="govuk-body"]'
     ).text.split()[0]
+
     assert int(total_results) < context.results
 
 
@@ -184,7 +195,7 @@ def step_impl_resulting_generic_search(context, location):
     context.execute_steps(
         f"""
         Given I provide the "{location}" details
-        And I select the 'search' button on the FALA homepage
+        And I select the "search" button on the FALA homepage
         And I am taken to the page corresponding to "{location}" result
     """
     )
