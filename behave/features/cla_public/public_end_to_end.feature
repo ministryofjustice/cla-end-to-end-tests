de9d759c0a287ad9fb6f9ad37134d006d1b8efc9
Sarahyjja <sarahyjja@github.com>
2023-09-03 13:46:47 +0100

    LGA-2604: ✅  Implement steps to the exit behaviour

--- behave/features/cla_public/public_end_to_end.feature
+++ behave/features/cla_public/public_end_to_end.feature
@@ -3,19 +3,19 @@ Feature:
   - Applying for legal aid on the "Check if I can get legal aid" website
 
 Background: Start page
-    Given I am applying for legal aid on the "Check if I can get legal aid" website
+    Given I am on the "Check if I can get legal aid" website page
     And I have selected the start now button on the start page
-    And I should be on the diagnosis page "Choose the area you most need help with"
+    And I should be on the general diagnosis page "Choose the area you most need help with"
 
 @exit-behaviour-button, @a11y-check
-Scenario: Describes how to exit from the domestic abuse section and the website safely when clicking a button
+Scenario: Describes how to exit from the domestic abuse section and the website safely when clicking the exit button
   Given I select the "Domestic abuse" section
   And I should be on the "Choose the option that best describes your personal situation" page
   Then I click on the "Exit this page" button
   And I am diverted to the BBC website
 
 @exit-behaviour-keypress, @a11y-check
-Scenario: Describes how to exit from the domestic abuse section and the website safely when pressing a key
+Scenario: Describes how to exit from the domestic abuse section and the website safely when pressing the escape key
   Given I select the "Domestic abuse" section
   And I should be on the "Choose the option that best describes your personal situation" page
   Then I press the "esc" key on the keyboard
--- behave/features/steps/public_end_to_end.py
+++ behave/features/steps/public_end_to_end.py
@@ -1,5 +1,23 @@
 from behave import step
+
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+@step('I am on the "{homepage_title}" website page')
+def step_impl_homepage_title(context, homepage_title):
+    homepage_title_xpath = context.helperfunc.find_by_xpath("//main/div[2]/h1")
+    assert homepage_title_xpath == homepage_title
+
+
+@step('I should be on the general diagnosis page "{main_diagnosis_page_title}"')
+def step_impl_main_diagnosis_title(context, main_diagnosis_page_title):
+    main_diagnosis_page_title_xpath = context.helperfunc.find_by_xpath(
+        "//main/div[2]/fieldset/legend/h1"
+    )
+    assert main_diagnosis_page_title_xpath == main_diagnosis_page_title
 
 
 @step('I select the "{section_name}" section')
@@ -18,3 +36,21 @@ def step_impl_click_exit_page(context):
     context.helperfunc.find_by_xpath(
         '//html/body/div[3][@class="govuk-exit-this-page"]'
     )
+
+
+@step('I press the "esc" key on the keyboard')
+def step_impl_press_esc_key(context):
+    context.helperfunc.find_element_by_class_name(".govuk-exit-this-page")
+    ActionChains(webdriver).send_keys(Keys.ESCAPE).perform()
+
+
+@step("I am diverted to the BBC website")
+def step_impl_diversion_link(context, link_text):
+    assert (
+        context.helperfunc.find_by_link_text(link_text) is not None
+    ), f"Could not find link: {link_text}"
+
+    link_href = context.helperfunc.find_by_link_text(link_text).get_attribute("href")
+    context.helperfunc.click_button(By.LINK_TEXT, link_text)
+
+    return link_href
