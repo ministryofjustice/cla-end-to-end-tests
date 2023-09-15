3ed4cca720e240b637ed32e0bc593527d611d07f
Patrick Sweeting <patrick.sweeting@digital.justice.gov.uk>
2023-09-15 09:15:56 +0100

    LGA-2604 Resolving incorrect xpath to exit page button and assert err… (#120)
    * LGA-2604 Resolving incorrect xpath to exit page button and assert error message added to allow visual compare
    
    * LGA-2604 Adding BBC international URL and updating assert for URL check
    
    * LGA-2604 Updating assert message to only print URL of page

--- behave/features/steps/public_exit_page.py
+++ behave/features/steps/public_exit_page.py
@@ -4,13 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-from helper.constants import BBC_WEBSITE
+from helper.constants import BBC_WEBSITE, BBC_INTERNATIONAL_WEBSITE
 
 
 @step('The "Exit this page" button is on the page and I click it')
 def step_impl_click_exit_page(context):
     href_bbc_address = context.helperfunc.find_by_xpath(
-        "//main/div/div/a[contains(text(), 'Exit this page')]"
+        "//main/div/a[contains(text(), 'Exit this page')]"
     ).get_attribute("href")
 
     assert href_bbc_address == BBC_WEBSITE
@@ -34,4 +34,8 @@ def step_impl_keypress_multiple_times(context, keypress, amount_of_time):
 
 @step("I am diverted to the BBC website")
 def step_impl_diversion_link(context):
-    assert context.helperfunc.get_url() == BBC_WEBSITE
+    # Depending on where in the world the test is executed you will either be directed to bbc.co.uk or bbc.com
+    assert context.helperfunc.get_url() in [
+        BBC_WEBSITE,
+        BBC_INTERNATIONAL_WEBSITE,
+    ], f"Url is {context.helperfunc.get_url()}"
