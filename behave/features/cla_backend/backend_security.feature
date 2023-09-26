2754587a8b9d06971e135ad0e9a37b92571dc954
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2023-09-26 11:26:20 +0100

    Add in tests

--- behave/features/cla_backend/backend_security.feature
+++ behave/features/cla_backend/backend_security.feature
@@ -2,24 +2,29 @@
 Feature: Security checks to ensure CLA_Backend timesouts
 
 Background: Log in to the Fox admin as a user within the super users group
-    Given I am logged in as "FOX_ADMIN_GENERAL_USER"
+    Given I am logged in as "FOX_ADMIN_SECURITY_USER"
 
 @security @inactivity
 Scenario: After a set period of time, CLA Backend should timeout from inactivity
     Given The session warning is not visible
     And I wait for the session warning
-    Then The timeout warning should appear
     And I wait for the page to timeout
     Then I am logged out
 
 @security @sessionend
 Scenario: Confirm that the session ends on browser close
+    Given I close the browser
+    And I open a new CHS browser
+    Then I am logged out
 
+    
 @security @inactivity @passiveurl
 Scenario: Confirm that the session timesout on passive urls
-
-
-
+    Given I am on a passive URL
+    And The session warning is not visible
+    And And I wait for the session warning
+    And I wait for the page to timeout
+    Then I am logged out
 
 
 
--- behave/features/steps/backend_security.py
+++ behave/features/steps/backend_security.py
@@ -1,29 +1,58 @@
 from behave import step
 from selenium.webdriver.common.by import By
-import datetime
 import logging
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-import os
+from selenium.common.exceptions import TimeoutException
 from helper.constants import (
-    MINIMUM_WAIT_UNTIL_TIME,
-    CLA_BACKEND_USER_TO_ASSIGN_STATUS_TO,
-    CLA_BACKEND_USER_TO_ASSIGN_STATUS_TO_PK,
-    FOX_ADMIN_FORM_FIELDS,
-    USERS,
+    CLA_BACKEND_SECURITY_URL,
 )
-from features.steps.common_steps import wait_until_page_is_loaded, assert_header_on_page
+
 
 @step("The session warning is not visible")
 def step_impl_session_warning_not_visible(context):
-    warning = context.helperfunc.find_by_id('session_security_warning')
-    assert warning.getCSSValue("display") == None
+    try:
+        WebDriverWait(context.helperfunc.driver(), 5).until(
+            EC.invisibility_of_element((By.ID, "session_security_warning"))
+        )
+    except TimeoutException:
+        logging.error("Warning message present during active session")
+
 
 @step("I wait for the session warning")
 def step_impl_wait_session_warnings(context):
     try:
-        WebDriverWait(context.browser, 9999).until(
-        EC.visibility_of_element_located((By.ID, "session_security_warning")))
+        WebDriverWait(context.helperfunc.driver(), 5).until(
+            EC.visibility_of_element_located((By.ID, "session_security_warning"))
+        )
     except TimeoutException:
         logging.error("Warning message not displayed after inactivity")
+
+
+@step("I wait for the page to timeout")
+def step_impl_session_timeout(context):
+    WebDriverWait(context.helperfunc.driver(), 30).until(
+        EC.visibility_of_element_located((By.ID, "id_username"))
+    )
+
+
+@step("I am logged out")
+def step_impl_logged_out(context):
+    assert WebDriverWait(context.helperfunc.driver(), 30).until(
+        EC.visibility_of_element_located((By.ID, "id_password"))
+    )
+
+
+@step("I close the browser")
+def step_impl_close_browser(context):
+    context.helperfunc.close()
+
+
+@step("I open a new CHS browser")
+def step_impl_open_chs_browser(context):
+    context.helperfunc.open(CLA_BACKEND_SECURITY_URL)
+
+
+@step("I am on a passive URL")
+def step_impl_open_passive_url(context):
+    context.helperfunc.open(CLA_BACKEND_SECURITY_URL & "/reports/mi-cb1-extract/")
