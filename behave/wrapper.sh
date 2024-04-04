63889026f3ebdab15a9347a82033e20d42bb35b8
Stephen Dicks <github@starswan.com>
2024-04-04 13:38:10 +0100

    add FALA_TESTS_ONLY config variable

--- behave/wrapper.sh
+++ behave/wrapper.sh
@@ -3,12 +3,20 @@
 # Simple wrapper for executing behave within Docker.
 
 export A11Y=${A11Y_ENABLED:-false}
-echo "A11Y is $A11Y"
+echo "saying that A11Y is $A11Y"
+
+TAGS=""
+# Run only FALA tagged tests
+if [ "$FALA_TESTS_ONLY" = "true" ]
+then
+  TAGS="--tags=@fala"
+fi
+echo "TAGS is $TAGS"
 
 #
-# execute behave
+# execute behave with flags
 #
 
-exec behave "-D a11y=$A11Y"
+exec behave "-D a11y=$A11Y" $TAGS
 # Clean up downloads
 rm -rf data/downloads/*
