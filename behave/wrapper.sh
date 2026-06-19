a6b482bf42859cca3e85127ad37817a2c0d3f425
joselopez-moj <jose.lopez@justice.gov.uk>
2026-06-19 12:05:48 +0100

    infra: add logs for testing

--- behave/wrapper.sh
+++ behave/wrapper.sh
@@ -6,6 +6,7 @@ export A11Y=${A11Y_ENABLED:-false}
 echo "A11Y is $A11Y"
 
 
+/usr/local/bin/python3 -c "import sys; print('Python:', sys.executable, sys.version); import pkg_resources; print('pkg_resources OK')" || echo "pkg_resources FAILED"
 exec /usr/local/bin/python3 -m behave "-D a11y=$A11Y"
 # Clean up downloads
 rm -rf data/downloads/*
